7a391f9ed71547c3ad31ee24ae9d8b61b148a430
czh020110 <czh020110@163.com>
2024-07-07 16:11:19 +0800

    1

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -550,6 +550,20 @@ export function ChatActions(props: {
         icon={<MaskIcon />}
       />
 
+      <ChatAction
+        text={Locale.Chat.InputActions.Clear}
+        icon={<BreakIcon />}
+        onClick={() => {
+          chatStore.updateCurrentSession((session) => {
+            if (session.clearContextIndex === session.messages.length) {
+              session.clearContextIndex = undefined;
+            } else {
+              session.clearContextIndex = session.messages.length;
+              session.memoryPrompt = ""; // will clear memory
+            }
+          });
+        }}
+      />
 
       <ChatAction
         onClick={() => setShowModelSelector(true)}
@@ -575,23 +589,6 @@ export function ChatActions(props: {
           }}
         />
       )}
-      </div>
-      <div>
-        <ChatAction
-          text={Locale.Chat.InputActions.Clear}
-          icon={<BreakIcon />}
-          onClick={() => {
-            chatStore.updateCurrentSession((session) => {
-              if (session.clearContextIndex === session.messages.length) {
-                session.clearContextIndex = undefined;
-              } else {
-                session.clearContextIndex = session.messages.length;
-                session.memoryPrompt = ""; // will clear memory
-              }
-            });
-          }}
-        />
-      </div>
     </div>
   );
 }

75422f5f9c80b77682e0d95e24f51a74a281747a
czh020110 <czh020110@163.com>
2024-07-07 23:54:40 +0800

    哈哈哈哈

--- app/masks/cn.ts
+++ app/masks/cn.ts
@@ -2,7 +2,7 @@ import { BuiltinMask } from "./typing";
 
 export const CN_MASKS: BuiltinMask[] = [
   {
-    avatar: "1f9d1-200d-1f3eb",
+    avatar: "1f605",
     name: "Ch-En Translator",
     context: [
       {
@@ -20,16 +20,120 @@ export const CN_MASKS: BuiltinMask[] = [
       presence_penalty: 0,
       frequency_penalty: 0,
       sendMemory: false,
-      historyMessageCount: 4,
+      historyMessageCount: 6,
       compressMessageLengthThreshold: 1000,
     },
     lang: "cn",
     builtin: true,
-    createdAt: 1688899480524,
+    createdAt: 1688899480523,
   },
-  
+    {
+    avatar: "1f612",
+    name: "English Essay Writing Tutor",
+    context: [
+      {
+        id: "writer-0",
+        role: "user",
+        content:
+        "I want you to be a English Essay Writing Tutor. I will provide you with a student who needs help improving their writing. Your task is to use AI tools (such as natural language processing) to give the student feedback on how to improve their essays. You should also use your rhetorical knowledge and experience in effective writing techniques to suggest ways the student can better express their ideas in writing. Replace the original low-level vocabulary with more legant, refined, sophisticated, and more literary words, phrases, or sentences without changing the fluency of the sentences. My writing request or the essay that needs improvement is (Reply in Chinese for other matters):\n",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-4o",
+      temperature: 1,
+      max_tokens: 20000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: false,
+      historyMessageCount: 10,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480522,
+  },
+     {
+    avatar: "1f61c",
+    name: "English-English Dictionary (with Chinese explanations)",
+    context: [
+      {
+        id: "dictionary-0",
+        role: "user",
+        content:
+          "Convert the English word into a complete explanation that includes the Chinese translation, English definition, and three example sentences,and you need to research its origin and trace its roots. If applicable, you should also provide information on how the meaning of the word has changed over time. Please ensure all information is accurate and keep the response concise, without any additional feedback. The first word is (Reply in Chinese for other matters)",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-4o",
+      temperature: 1,
+      max_tokens: 20000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: false,
+      historyMessageCount: 6,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480529,
+  },
+
+   {
+    avatar: "1f60a",
+    name: "OCR",
+    context: [
+      {
+        id: "ocr-0",
+        role: "user",
+        content:
+          "I need you to act as an OCR, recognizing all the content in the image without adding any explanations, strictly following the layout format of the text in the image, and making special treatments only in the following situations.① When handling line breaks, use markdown format: add 2 spaces + newline character at the end of different lines, and use two newline characters for different paragraphs.② All math-related formulas (including formulas like f(x) that can be directly input) should be entirely in markdown format (e.g., $math formula$ in between) (you must check if the symbols are correctly recognized during identification).③ Code should be in code block format, such as ```code```.④ Except for the above situations, do not add markdown code symbols, such as title symbol #, bold symbol *, etc.",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-4o",
+      temperature: 1,
+      max_tokens: 20000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: false,
+      historyMessageCount: 6,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480521,
+  },
+     {
+    avatar: "1f602",
+    name: "Debater",
+    context: [
+      {
+        id: "debater-0",
+        role: "user",
+        content:
+          "I want you to play the role of a debater. I will provide you with some topics related to current events. Your task is to research both sides of the debate, present effective arguments for each side, refute opposing viewpoints, and draw persuasive conclusions based on evidence. Your goal is to help people gain knowledge and insight into the topic at hand through the discussion. Now, based on my prompts, start the debate（Reply in Chinese）",
+        date: "",
+      },
+    ],
+    modelConfig: {
+      model: "gpt-4o",
+      temperature: 1,
+      max_tokens: 20000,
+      presence_penalty: 0,
+      frequency_penalty: 0,
+      sendMemory: false,
+      historyMessageCount: 6,
+      compressMessageLengthThreshold: 1000,
+    },
+    lang: "cn",
+    builtin: true,
+    createdAt: 1688899480520,
+  },
   {
-    avatar: "270d-fe0f",
+    avatar: "1f617",
     name: "Internet writer",
     context: [
       {
@@ -54,12 +158,13 @@ export const CN_MASKS: BuiltinMask[] = [
       presence_penalty: 0,
       frequency_penalty: 0,
       sendMemory: false,
-      historyMessageCount: 4,
+      historyMessageCount: 6,
       compressMessageLengthThreshold: 1000,
     },
     lang: "cn",
     builtin: true,
     createdAt: 1688899480537,
   },
+  
 
 ];
